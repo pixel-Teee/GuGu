811f121cde8fec421924d01f1a3754a9570cdeeb
pixel-Teee <47231777+pixel-Teee@users.noreply.github.com>
2025-03-02 17:02:54 +0800

    Update IDetailsView.h

--- editor/Editor/PropertyEditor/IDetailsView.h
+++ editor/Editor/PropertyEditor/IDetailsView.h
@@ -3,9 +3,10 @@
 #include <Core/UI/CompoundWidget.h>
 
 namespace GuGu {
+	class GameObject;//game object
 	class IDetailsView : public CompoundWidget
 	{
 	public:
-
+		virtual void setObjects(const std::vector<GameObject*>& inObjects, bool bForceRefresh = false, bool bOverrideLock = false) = 0;
 	};
 }
