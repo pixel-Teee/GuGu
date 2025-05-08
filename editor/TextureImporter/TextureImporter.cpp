2971bf6681d1ef2ab919fb5629aa0f2e358447e5
pixel-Teee <47231777+pixel-Teee@users.noreply.github.com>
2025-05-08 20:38:30 +0800

    add texture importer

---
+++ editor/TextureImporter/TextureImporter.cpp
@@ -0,0 +1,17 @@
+#include <pch.h>
+
+#include "TextureImporter.h"
+
+namespace GuGu {
+
+	TextureImporter::TextureImporter()
+	{
+
+	}
+
+	TextureImporter::~TextureImporter()
+	{
+
+	}
+
+}
---
+++ editor/TextureImporter/TextureImporter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace GuGu {
+	class TextureImporter
+	{
+	public:
+		TextureImporter();
+
+		virtual ~TextureImporter();
+	};
+}
