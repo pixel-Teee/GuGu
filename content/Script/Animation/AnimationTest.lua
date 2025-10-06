44ec622739f7a41a5336c12429cc8d32fb32f616
pixel-Teee <47231777+pixel-Teee@users.noreply.github.com>
2025-10-07 01:10:44 +0800

    test animation

--- content/Script/Animation/AnimationTest.lua
+++ content/Script/Animation/AnimationTest.lua
@@ -5,6 +5,7 @@ function AnimationTest:init(owner)
 	self.owner = owner
     self.animator = GuGu.GAnimator() --create animator
     self.animation = self.owner:getWorld():loadAnimation("content/Defeated_anim.json")
+    self.dacingAnimation = self.owner:getWorld():loadAnimation("content/Rumba Dancing_anim.json")
     self.staticMesh = self.owner:getComponent("GuGu::StaticMeshComponent")
     --get mesh
     self.animator:playAnimationWithAsset(self.animation, self.staticMesh:getStaticMeshAsset())
@@ -20,6 +21,16 @@ function AnimationTest:init(owner)
 			textComponent:setText("Stop")
             self.textComponent = textComponent
 		end 
+        self.dacingButton = currentLevel:getGameObject("PlayDacingButton")
+        buttonComponent =  self.dacingButton:getComponent("GuGu::ButtonComponent")
+		if buttonComponent then
+			buttonComponent.m_onClicked:addFunction(self.owner:getComponent("GuGu::ScriptComponent"), "OnPlayDacingButtonClick")
+		end 
+        textComponent = currentLevel:getGameObject("PlayDacingButton_Text"):getComponent("GuGu::TextComponent")
+        if textComponent then
+			textComponent:setText("PlayDacing")
+            self.dacingButtonTextComponent = textComponent
+		end 
     end
 end
 
@@ -34,6 +45,16 @@ function AnimationTest:OnPlayButtonClick()
     else
         self.animator:playAnimationWithAsset(self.animation, self.staticMesh:getStaticMeshAsset())
         self.textComponent:setText("Stop")
+    end
+end
+
+function AnimationTest:OnPlayDacingButtonClick()
+    if self.animator:isRunning() then
+        self.animator:stopAnimation()
+        self.dacingButtonTextComponent:setText("PlayDacing")
+    else
+        self.animator:playAnimationWithAsset(self.dacingAnimation, self.staticMesh:getStaticMeshAsset())
+        self.dacingButtonTextComponent:setText("StopDacing")
     end
 end
 
