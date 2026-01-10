018581d85bcbb4fa57397926a46edd3d4920163c
pixel-Teee <47231777+pixel-Teee@users.noreply.github.com>
2026-01-10 20:46:54 +0800

    add physics

---
+++ src/Core/Physics/PhysicsManager.cpp
@@ -0,0 +1,17 @@
+#include <pch.h>
+
+#include "PhysicsManager.h"
+
+namespace GuGu {
+
+	PhysicsManager::PhysicsManager()
+	{
+
+	}
+
+	PhysicsManager::~PhysicsManager()
+	{
+
+	}
+
+}
---
+++ src/Core/Physics/PhysicsManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <btBulletDynamicsCommon.h>
+
+namespace GuGu {
+	//physics manager
+	class PhysicsManager
+	{
+	public:
+		PhysicsManager();
+
+		~PhysicsManager();
+	};
+}
